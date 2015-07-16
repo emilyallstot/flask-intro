e4b0ac9683380fbca42150c16a1d95caf116f4f7
Hackbright Student <info@hackbrightacademy.com>
2015-07-16 13:07:26 -0700

    what is wrong with post????

--- nice.py
+++ nice.py
@@ -10,7 +10,17 @@ app = Flask(__name__)
 # route to handle the landing page of a website.
 @app.route('/')
 def start_here():
-    return "Hi! This is the home page."
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head></head>
+        <body>
+            !Hi! This is the home page." 
+            <a href='/hello'>Click me!</a>
+        </body>
+    </html>
+
+    """
 
 # route to display a simple web page
 @app.route('/hello')
@@ -23,24 +33,45 @@ def say_hello():
         </head>
         <body>
             <h1>Hi There!</h1>
-            <form action="/greet">
+            <form action="/greet", method="POST">
                 <label>What's your name? <input type="text" name="person"></label>
                 <input type="submit">
+                
+                <br>
+                <br>
+
+                <label>Select your compliment: 
+                <input type="radio" name="AWESOMENESS" value="awesome">Awesome
+                <input type="radio" name="AWESOMENESS" value="terrific">Terrific
+                <input type="radio" name="AWESOMENESS" value="fantastic">Fantastic
+                <input type="radio" name="AWESOMENESS" value="neato">Neato
+                <input type="radio" name="AWESOMENESS" value="fantabulous">Fantabulous
+                <input type="radio" name="AWESOMENESS" value="wowza">Wowza
+                <input type="radio" name="AWESOMENESS" value="oh-so-not-meh">Oh-so-not-meh
+                <input type="radio" name="AWESOMENESS" value="brilliant">Brilliant
+                <input type="radio" name="AWESOMENESS" value="ducky">Ducky
+                <input type="radio" name="AWESOMENESS" value="coolio">Coolio
+                <input type="radio" name="AWESOMENESS" value="incredible">Incredible
+                <input type="radio" name="AWESOMENESS" value="wonderful">Wonderful
+                <input type="radio" name="AWESOMENESS" value="smashing">Smashing
+                <input type="radio" name="AWESOMENESS" value="lovely">Lovely
+                </label>
             </form>
         </body>
     </html>
 
     """
 
-@app.route('/greet')
+@app.route('/greet', methods=['post'])
 def greet_person():
     player = request.args.get("person")
+    compliment = request.args.get("AWESOMENESS")
 
-    AWESOMENESS = [
-        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
-        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+    # AWESOMENESS = [
+    #     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    #     'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
 
-    compliment = choice(AWESOMENESS)
+    # compliment = choice(AWESOMENESS)
 
     return """
     <!DOCTYPE html>
